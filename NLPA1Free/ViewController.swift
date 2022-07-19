0d192d51cdd7cc9a77e2c089289523b3ed460f5a
Shen Jun Wu <shenfive@gmail.com>
2022-07-19 11:43:27 +0800

    Initial Commit

---
+++ NLPA1Free/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NLPA1Free
+//
+//  Created by 申潤五 on 2022/7/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
